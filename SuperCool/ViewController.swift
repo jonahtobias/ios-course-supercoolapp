35ff4e4f820497796992b03c4eecf3ba799d3386
Jonah Tobias <jonah@jonahtobias.com>
2015-10-19 09:03:32 -0400

    added assets and updated UI

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeSuperCool(sender: AnyObject) {
+        
+        textImage.hidden = !textImage.hidden
+        
+        
+        
+    }
 
 }
 
